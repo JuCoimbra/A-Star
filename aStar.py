82ba32ffb6a81d5f1171701f2a43fae2f6fbf0fa
Juliane Coimbra <julianearc@gmail.com>
2022-09-13 16:59:57 -0300

    Update aStar.py

--- aStar.py
+++ aStar.py
@@ -1,4 +1,5 @@
 from operator import itemgetter as itemgetter
+from weakref import finalize
 
 percurso = {"Salvador" : {"TempoL":346, "Destinos":["BR-324"], "TempoD":[79], "f": 0, "Previous":"null"},
             "BR-324" : {"TempoL":327, "Destinos":["Feira de Santana", "Sapeaçu"], "TempoD":[28, 64], "f": 0, "Previous":"null"},
@@ -9,59 +10,83 @@ percurso = {"Salvador" : {"TempoL":346, "Destinos":["BR-324"], "TempoD":[79], "f
             "Iaçu": {"TempoL":125, "Destinos":["Itaberaba", "Crispim"], "TempoD":[29, 62], "f": 0, "Previous":"null"},
             "Crispim":{"TempoL":150, "Destinos":["Boa Vista do Tupim"], "TempoD":[55], "f": 0, "Previous":"null"},
             "Boa Vista do Tupim":{"TempoL":180, "Destinos":["Lençois"], "TempoD":[120], "f": 0, "Previous":"null"},
-            "Itaberaba":{"TempoL":120, "Destinos":["Lençois"], "TempoD":[125], "f": 0, "Previous":"null"}}
+            "Itaberaba":{"TempoL":120, "Destinos":["Lençois"], "TempoD":[125], "f": 0, "Previous":"null"},
+            "Lençois":{"TempoL":0, "Destinos":[], "TempoD":[], "f": 0, "Previous":"null"}}
 
 #Calcula o valor f (TempoL + Heuristica) para o nó passado
 def antPercu(vertex, antVertex):
+
     if vertex != 'Salvador':
-        indexAntn = percurso[vertex]["Destinos"].index(antVertex)
-        f = percurso[vertex]["TempoD"][indexAntn] + antPercu(percurso[vertex]["Previous"], vertex)
+        indexAntn = percurso[antVertex]["Destinos"].index(vertex)
+        f = percurso[antVertex]["TempoD"][indexAntn] + antPercu(antVertex, percurso[antVertex]["Previous"])
         return f
     else:
-        return percurso[vertex]["TempoL"]
-
+        return percurso[vertex]["TempoD"][0]
 
 #Adiciona a lista acessar(lista "open" no slide) os nós vizinhos ao atual, sempre atualizando o estado f deles.
 def addAcessar(atuVertex, acessar):
-    for vertex in percurso[atuVertex]["Destinos"]:
-        f = antPercu(vertex, percurso[vertex]["Previous"])
 
-        if (percurso[vertex]["f"] == 0) or (percurso[vertex]["f"] > f):
+    for vertex in percurso[atuVertex]["Destinos"]:
+
+        f = antPercu(vertex, atuVertex)
+
+        if (percurso[vertex]["f"] == 0):
             percurso[vertex]["f"] = f
             percurso[vertex]["Previous"] = atuVertex
-        
-        acessar = acessar.append(percurso[vertex])
-        
-    return sorted(acessar, key = itemgetter("f"))
+            acessar.append((vertex, percurso[vertex]["f"]))
+            
+        elif percurso[vertex]["f"] > f:
+            antF = percurso[vertex]["f"]
+            percurso[vertex]["f"] = f
+            percurso[vertex]["Previous"] = atuVertex
+            acessar.append((vertex, percurso[vertex]["f"]))
+            
+            if (vertex, antF) in acessar:
+                acessar.remove((vertex, antF))
+
+    return sorted(acessar, key=lambda i: i[1])
 
 #Monta o caminho da origem ao destino
 def way(vertex, finalWay):
+
     if vertex != "Salvador":
-        finalWay = finalWay.append(vertex)
+        finalWay.append((vertex, percurso[vertex]["f"]))
         return way(percurso[vertex]["Previous"], finalWay)
+
     else:
-        finalWay.append("Salvador")
-        return finalWay.reverse()
+        finalWay.append(("Salvador", percurso["Salvador"]["f"]))
+        finalWay.reverse()
+        return finalWay
 
 
 def aStar(origin, destino):
     acessados = []
-    acessar = [origin]
-
+    finalWay = []
+    
     percurso[origin]["f"] = antPercu(origin, "null")
+    percurso[origin]["Previous"] = "null"
 
-    for vertex in acessar:
-        acessar = addAcessar(vertex, acessar)
-        acessados = acessados.append(vertex)
+    acessar = [(origin, percurso[origin]['f'])]
 
-        if vertex == destino:
-            finalWay = way(vertex, finalWay)
-
-    
-
-    
-
+    while acessar:
         
+        if acessar[0][0] != destino:
+
+            acessados.append((acessar[0][0], acessar[0][1]))
+            acessar = addAcessar(acessar[0][0], acessar)
+            acessar.remove(acessados[len(acessados)-1])
+
+        else:
+
+            acessados.append((acessar[0][0], acessar[0][1]))
+            acessar.remove(acessados[len(acessados)-1])
+
+    finalWay = way(destino, finalWay)
+    print("O melhor caminho é:")
+    print(finalWay)
+
+if __name__ == '__main__':
+    aStar("Salvador", "Lençois")
 
 
     
